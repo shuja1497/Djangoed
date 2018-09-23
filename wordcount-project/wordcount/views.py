c9bc361a6c35eac7ae2fcc77ed7d7fe342fe824a
shuja1497 <shujareshi1497@gmail.com>
2018-09-24 00:59:48 +0530

    word counting done.

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 
 def home(request):
@@ -11,4 +12,16 @@ def barcelona(request):
 
 
 def count(request):
-    return HttpResponse('Count')
+    text = request.GET['fulltext']
+    wordlist = text.split()
+    worddictionary = {}
+
+    for words in wordlist:
+        if words in worddictionary:
+            worddictionary[words] += 1
+        else:
+            worddictionary[words] = 1
+
+    sorted_words = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext': text, 'count': len(wordlist), 'sorted_words': sorted_words})
