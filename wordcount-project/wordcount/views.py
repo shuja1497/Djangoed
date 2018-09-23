84f4d1e120b4a8a27ec5c7cc37737d137c710868
shuja1497 <shujareshi1497@gmail.com>
2018-09-23 20:09:36 +0530

    added templates.

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, 'home.html', {"football": "the best"})
 
 
 def barcelona(request):
