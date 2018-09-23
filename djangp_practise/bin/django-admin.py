3690f01c4a7bd9052768b5cbff9f1f4372f67668
shuja1497 <shujareshi1497@gmail.com>
2018-09-23 19:22:40 +0530

    created first django project

---
+++ djangp_practise/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shuja1497/study/Django2-Practise/Djangoed/djangp_practise/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
