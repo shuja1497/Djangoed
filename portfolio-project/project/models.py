5e5b70e12c3b3262e5df285786dde8d591b9e5f8
Shuja Reshi <shujareshi1497@gmail.com>
2018-10-02 03:01:42 +0530

    created project model and applied migrations.

--- portfolio-project/project/models.py
+++ portfolio-project/project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    image = models.ImageField(upload_to='images')
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=200)
+    link = models.CharField(max_length=100)
