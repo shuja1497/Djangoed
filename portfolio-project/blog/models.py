e25fd501abf8b79f9c9deed5949eb882531f08c2
Shuja Reshi <shujareshi1497@gmail.com>
2018-10-16 04:29:12 +0530

    blogs app working

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
