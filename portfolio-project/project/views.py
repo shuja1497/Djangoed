a4af2418246bbf998129f449577f31b586dced56
Shuja Reshi <shujareshi1497@gmail.com>
2018-10-16 13:18:45 +0530

    project backend integrated with frontend

--- portfolio-project/project/views.py
+++ portfolio-project/project/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Project
+
 
 def home(request):
-    return render(request, 'project/homepage.html')
+    projects = Project.objects  # getting all list of projects from the database
+    return render(request, 'project/homepage.html', {'projects': projects})
