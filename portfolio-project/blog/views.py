40277c8cdab2ffa1674c4da8188ad387e5e3a80c
Shuja Reshi <shujareshi1497@gmail.com>
2018-11-04 02:27:27 +0530

    working with path parameters and static files

--- portfolio-project/blog/views.py
+++ portfolio-project/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -8,3 +8,8 @@ from .models import Blog
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+
+def detail_blogs(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': detail_blog})
--- portfolio-project/project/models.py
+++ portfolio-project/project/models.py
@@ -8,3 +8,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=200)
     link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
