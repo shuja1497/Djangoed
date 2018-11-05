8f159b84092044ecb290b22884b591f0b3658d1b
Shuja Reshi <shujareshi1497@gmail.com>
2018-11-05 17:37:30 +0530

    login logout  added.

--- producthunt-project/accounts/views.py
+++ producthunt-project/accounts/views.py
@@ -24,8 +24,18 @@ def signup(request):
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect!'})
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
