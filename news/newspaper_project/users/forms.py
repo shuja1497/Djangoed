18e1db623f672e7d67d4090ca38a654a7f084f91
Shuja Reshi <shujareshi1497@gmail.com>
2018-11-07 23:43:47 +0530

    Added user authentication - login logout and custom signup.

--- news/newspaper_project/users/forms.py
+++ news/newspaper_project/users/forms.py
@@ -6,7 +6,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 
 class CustomUserChangeForm(UserChangeForm):
