a883d63b00ad8211be79dcc4ad11f9cd53369ac3
Shuja Reshi <shujareshi1497@gmail.com>
2018-11-15 19:02:42 +0530

    basic setup for articles app done

--- news/newspaper_project/newspaper_project/settings.py
+++ news/newspaper_project/newspaper_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'pages',
+    'articles',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
