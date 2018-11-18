10a2d132285dd83a7aace93a161f590996daf127
Shuja Reshi <shujareshi1497@gmail.com>
2018-11-18 12:22:02 +0530

    update edit delete articles in place

--- news/newspaper_project/articles/urls.py
+++ news/newspaper_project/articles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ArticlesListView
+from .views import ArticlesListView, ArticlesUpdateView, ArticlesDetailView, ArticlesDeleteView
 
 
 urlpatterns = [
     path('', ArticlesListView.as_view(), name='article_list'),
+    path('<int:pk>/edit', ArticlesUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/delete', ArticlesDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/', ArticlesDetailView.as_view(), name='article_detail'),
 ]
--- news/newspaper_project/articles/views.py
+++ news/newspaper_project/articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 from .models import Article
 # Create your views here.
 
@@ -7,5 +9,23 @@ from .models import Article
 class ArticlesListView(ListView):
     model = Article
     template_name = "article_list.html"
+
+
+class ArticlesDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+
+class ArticlesUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body')
+    template_name = 'article_edit.html'
+
+
+class ArticlesDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
 
 
